01b895e24b598c48db516cc1ad42a0d57697e7b6
tgYun783 <163853517+tgYun783@users.noreply.github.com>
2025-09-12 15:49:58 +0900

    [Bronze III] Title: 성장의 비약 선택권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26645. 성장의 비약 선택권/성장의 비약 선택권.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; 
+    cin >> n;
+    if (n < 206)
+        cout << 1;
+    else if(n < 218)
+        cout << 2;
+    else if(n < 229)
+        cout << 3;
+    else
+        cout << 4;
+}   
