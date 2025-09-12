ecfa19ea1b8c228e94c09c004dbcf5d0dfa9a4e1
tgYun783 <163853517+tgYun783@users.noreply.github.com>
2025-09-12 15:32:27 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long sum = 0, t;
+    for(int i=0; i<5; i++)
+    {
+        cin >> t;
+        sum += t;
+    }
+    cout << sum;
+
+}   
