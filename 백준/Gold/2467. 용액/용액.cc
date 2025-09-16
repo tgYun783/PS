145c59a91d85afaedef3a7cf0af3422d1459b6c8
tgYun783 <163853517+tgYun783@users.noreply.github.com>
2025-09-16 16:45:17 +0900

    [Gold V] Title: 용액, Time: 56 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int>V(n);
+  for(int i=0; i<n; i++)
+    cin >> V[i];
+  
+  int ans = 2e9 +1;
+  int left = 0;
+  int right = n-1;
+
+  int ans1;
+  int ans2;
+  while(right > left)
+  {
+    long long mid = V[left] + V[right];
+
+    if(abs(mid) < ans)
+    {
+      ans = abs(mid);
+      ans1 = left;
+      ans2 = right;
+    }
+
+    if(mid >=0)
+      right--;
+    else
+      left++;
+
+  }
+  cout << V[ans1] << " " << V[ans2];
+
+
+}
