d554c77cab7cf59d22ba0571c890190525a22f0c
tgYun783 <163853517+tgYun783@users.noreply.github.com>
2025-09-19 20:50:59 +0900

    [Gold III] Title: 소수의 연속합, Time: 24 ms, Memory: 5608 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<bool> prime;
+void erastotenes(int t)
+{
+  prime = vector<bool>(t+1,true);
+  prime[0] = false, prime[1] = false;
+  int _ = sqrt(t);
+  for(int n = 2; n <= _; n++)
+  {
+    if(prime[n])
+    {
+      for(int i = n*n; i <= t; i += n)
+        prime[i] = false;
+    }
+  }
+}
+
+int solution(int n)
+{
+  erastotenes(n);
+  vector<int>V;
+  for(int i=2; i<=n; i++)
+    if(prime[i])
+      V.push_back(i);
+  
+  int start = 0;
+  int end = -1;
+  int sum = 0;
+  int count = 0;
+  while(1)
+  {
+    if(end == V.size() -1 && sum < n)
+      break;
+    
+    if(sum < n)
+    {
+      
+      end++;
+      if(end >= V.size())
+        break;
+      sum += V[end];
+    }
+    
+    if(sum >= n)
+    {
+      if(sum == n)
+        count++;
+      sum -= V[start];
+      start++;
+    }
+  }
+  return count;
+}
+int main()
+{
+  int n;
+  cin >> n;
+  cout << solution(n);
+}
