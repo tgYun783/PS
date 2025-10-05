ba2a39306fee654ad3032f5fe3d345f7d9b59846
tgYun783 <163853517+tgYun783@users.noreply.github.com>
2025-10-05 14:47:06 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -6,19 +6,15 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
-    vector<vector<int>> v(12, vector<int>(4));
-    v[1][1] = 1;
-    v[2][1] = 1;
-    v[2][2] = 1;
-    v[3][1] = 2;
-    v[3][2] = 1;
-    v[3][3] = 1;
+    vector<int> v(12);
+    v[1] = 1;
+    v[2] = 2;
+    v[3] = 4;
     for (int i = 4; i <= 11; i++)
     {
-        v[i][1] = v[i - 1][1] + v[i - 1][2] + v[i - 1][3];
-        v[i][2] = v[i - 2][1] + v[i - 2][2] + v[i - 2][3];
-        v[i][3] = v[i - 3][1] + v[i - 3][2] + v[i - 3][3];
+        v[i] = v[i - 1] + v[i - 2] + v[i - 3];
     }
+
     int t;
     cin >> t;
     for (int i = 0; i < t; i++)
@@ -26,7 +22,7 @@ int main()
         int n, ans;
         cin >> n;
 
-        ans = v[n][1] + v[n][2] + v[n][3];
+        ans = v[n];
         cout << ans << '\n';
     }
 }
