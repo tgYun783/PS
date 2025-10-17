f125b89601ef28e7b13c86fe6054dd9c26206f44
tgYun783 <163853517+tgYun783@users.noreply.github.com>
2025-10-17 23:50:50 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 104 ms, Memory: 12356 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> ans;
+vector<vector<int>> v;
+vector<bool>visited;
+void dfs(int node)
+{
+    for(auto target : v[node])
+    {
+        if(visited[target] == false)
+        {
+            visited[target] = true;
+            ans[target] = node;
+            dfs(target);
+        }
+    }
+}
+int main()
+{
+   int n;
+   cin >> n;
+   v = vector<vector<int>>(n+1);
+   visited = vector<bool>(n+1, false);
+   visited[1] = true;
+   ans = vector<int>(n+1);
+
+   for(int i=0; i<n-1; i++)
+   {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+   }
+
+   dfs(1);
+   for(int i=2; i<ans.size(); i++)
+    cout << ans[i] << '\n';
+}
